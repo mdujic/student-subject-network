0de7bba9d9930e8b8ad0b4adbe1e6481784292f4
Jurica Horvat <jura959@gmail.com>
2022-06-09 18:14:47 +0200

    Neo4j recommendation

--- controller/subjectController.php
+++ controller/subjectController.php
@@ -14,14 +14,31 @@ class SubjectController extends BaseController
 		$ser = new Service();
 		if(isset($_GET['id_subject'])){
 			$subject = $ser->getSubjectById($_GET['id_subject']);
-			$title = $subject->subjectName;
+			$this -> registry -> template -> title = $subject->subjectName;
             $students = $ser->getStudentsOfSubject($subject->subjectID);
+            $this -> registry -> template -> students = $students;
 			$this->registry->template->show( 'one_subject' );
-	
             
 		}else{
 			$this->index();
 		}
+	}
+
+	public function mySubjects(){
+		$moj_jmbag = '1191242519';
+		$predmet = '92978';
+		$ser = new Service();
+		$my_subjects = $ser -> getMySubjects($moj_jmbag);
+			$this -> registry -> template -> predmeti = $my_subjects;
+		$this -> registry -> template -> ime_i_prezime = $moj_jmbag;
+		$preporuke = array();
+		foreach($my_subjects as $subj){
+			$best = $ser -> recommendMeSubject($moj_jmbag, $subj -> subjectID);
+			$best = $best[0];
+			$preporuke[] = $best;
+		}
+		$this -> registry -> template -> preporuka = $preporuke;
+		$this -> registry -> template -> show( 'one_student' );
 	}
 
 }
