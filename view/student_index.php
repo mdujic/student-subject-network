5a8e43161db6cae33b63d1faaa2b5f1186cc48f7
Jurica Horvat <jura959@gmail.com>
2022-06-13 01:28:18 +0200

    Radi unos studenata

--- view/student_index.php
+++ view/student_index.php
@@ -13,5 +13,37 @@
             }
         }
 	?>
-  </ul>
+    </ul>
+
+   <?php if($_SESSION['username'] === 'admin') { ?>
+
+        <h2>Unesi novog studenta: </h2>
+        <form method = "post" action = " <?php echo __SITE_URL . '/index.php?rt=student/insertStudent'?>">
+            <label>Ime: </label>
+            <input type="text" name="ime"/> <br/>
+            <label>Prezime: </label>
+            <input type="text" name="prezime"/> <br/>
+            <label>Email: </label>
+            <input type="text" name="email"/> <br/>
+            <label>JMBAG:</label>
+            <input type="text" name="jmbag"/> <br/>
+            <label>OIB: </label>
+            <input type="text" name="oib"/> <br/>
+            <label>Datum rođenja (MM/DD/GG): </label>
+            <input type = "text" name = "datum_rodenja"/> <br/>
+            <label>Mjesto rođenja: </label>
+            <input type = "text" name = "mjesto_rodenja"/> <br/>
+            <label for = "spol"> Spol: </label>
+            <select name = "spol" id = "spol">
+                <option id="spol" name="spol" value="M" selected>M</option>
+                <option id="spol" name="spol" value="Ž" >Ž</option>
+            </select>
+            <br />
+            <button type="submit">Unesi novog studenta!</button>
+        </form>
+        <br/>
+        <?php if(isset($message)){ echo '<p style = "color: ' . $tcolor . ';">' . $message . '</p>'; }?> 
+    <?php }?>
+
+
   <?php require_once __SITE_PATH . '/view/_footer.php'; ?>
